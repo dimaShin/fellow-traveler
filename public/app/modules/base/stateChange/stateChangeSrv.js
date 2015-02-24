d2f38f5fd052f0009edbbd16fb980214b57ca114
dimaShin <yadmitriy@gmail.com>
2015-02-24 16:21:39 +0200

    driver/traveler routes buttons and / route fix

--- public/app/modules/base/stateChange/stateChangeSrv.js
+++ public/app/modules/base/stateChange/stateChangeSrv.js
@@ -6,9 +6,10 @@ define([], function(){
     function stateChangeSrv($state, $timeout, $rootScope, authSrv, $urlRouter){
         return {
             stateWatcher: function stateWatcher(event, toState, stateParams, fromState){
-                event.preventDefault();
-                console.log('$stateChangeStart event fired! to state: ', toState.name, stateParams, fromState);
+                //event.preventDefault();
+                console.log('$stateChangeStart event fired! to state: ', toState);
                 if (toState.data && toState.data.secure) {
+                    event.preventDefault();
                     authSrv.isAuthorized()
                         .done(function () {
                             $state.go(toState.name, stateParams, {
@@ -23,15 +24,23 @@ define([], function(){
                                 $rootScope.$broadcast('$stateChangeSuccess', toState, stateParams);
                             });
                         })
-                } else {
-                    $state.go(toState.name, stateParams, {
-                        notify: false,
-                        location: 'reload',
-                        reload: true
-                    }).then(function () {
-                        $rootScope.$broadcast('$stateChangeSuccess', toState, stateParams);
-                    });
                 }
+                //else {
+                //    console.log('else');
+                //    //if(!toState.name) toState.name = 'login';
+                //    if(!toState.name){
+                //        $state.go('login');
+                //    }else{
+                //        $state.go(toState.name, stateParams, {
+                //            notify: false,
+                //            location: 'reload',
+                //            reload: true
+                //        }).then(function () {
+                //            $rootScope.$broadcast('$stateChangeSuccess', toState, stateParams);
+                //        });
+                //    }
+                //
+                //}
             }
         }
     }
