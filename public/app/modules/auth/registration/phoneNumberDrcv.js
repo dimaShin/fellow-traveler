753a1eada92109051e467d91bda392979d2bb810
dimaShin <yadmitriy@gmail.com>
2015-03-03 17:59:59 +0200

    add custom mask to phoneD

--- public/app/modules/auth/registration/phoneNumberDrcv.js
+++ public/app/modules/auth/registration/phoneNumberDrcv.js
@@ -7,64 +7,72 @@ define([], function(){
             return value.replace(/\D/g, '');
         }
 
-        function putSpecChars(value){
-            var formatted = '+',
-                chars = {
-                    3: ' (',
-                    5: ') ',
-                    8: '-',
-                    10: '-'
-                }, i = 0;
+        function putSpecChars(value, chars){
+            var formatted = '', i = 0;
+            console.log('chars:', chars);
+            chars = chars || {
+                0: '+',
+                3: ' (',
+                5: ') ',
+                8: '-',
+                10: '-'
+            };
 
             value += '';
             while(value.length){
                 if(chars[i]){
-                    formatted += chars[i] + value[0];
+                    formatted += chars[i];
                 }else{
                     formatted += value[0];
+                    value = value.substr(1);
                 }
-                value = value.substr(1);
                 i++;
             }
 
             return formatted;
         }
-
         return {
             restrict: 'A',
             scope: true,
             require: 'ngModel',
             link: function($scope, el, attr, ctrl){
-                var lastValue, lastCaretPos;
+                var lastValue, lastCaretPos,
+                    mask = attr.phoneNumber,
+                    chars = {};
+                for(var i = 0; i < mask.length; i++){
+                    if(mask[i] !== 'd'){
+                        chars[i] = mask[i];
+                    }
+                }
+                console.log('mask: ', chars);
                 ctrl.$parsers.push(replaceNotDigits);
                 ctrl.$formatters.push(function(value){
-                    console.log('formatter: ', value);
                     if(value){
                         value += '';
                         value = replaceNotDigits(value);
-                        return putSpecChars(value);
+                        return putSpecChars(value, chars);
                     }
 
-                })
-                el.on('focus', function(){
-                    if(el.val() === ''){
-                        el.val('+');
-                        el[0].setSelectionRange(2, 2);
-                    }
-                })
+                });
+                //el.on('focus', function(){
+                //    if(el.val() === ''){
+                //        el.val('+');
+                //        el[0].setSelectionRange(2, 2);
+                //    }
+                //})
                 el.on('input', function(e){
                     var lastLength, numbers, formattedVal, newCharsCount, inputLength;
 
                     lastLength = el.val().length;
                     numbers = replaceNotDigits(el.val());
                     if(numbers.length > 12) numbers = numbers.substr(0, 12);
-                    formattedVal = putSpecChars(numbers);
+                    formattedVal = putSpecChars(numbers, chars);
                     newCharsCount = formattedVal.length - lastLength;
 
                     lastCaretPos = el[0].selectionEnd;
                     inputLength = !!lastValue
                         ? numbers.length - lastValue.length < 0
-                            ? 0 : numbers.length - lastValue.length
+                        ? 0 : numbers.length - lastValue.length
                         : numbers.length;
                     var caretPos = lastCaretPos + inputLength + newCharsCount;
                     lastValue = numbers;
